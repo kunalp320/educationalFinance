a52aa511d142e903931d45464be260b6cdb00e75
Kunal Patel <kpatel@Kunals-MacBook-Pro.local>
2016-02-23 13:13:41 -0800

    Temp .. still not working

--- stockQuote.js
+++ stockQuote.js
@@ -3,9 +3,13 @@ var app = angular.module('stockQuote', []);
 app.controller('StockQuoteController', ['$scope', '$http', function($scope, $http) {
   var stock = this;
   this.stockQuote = {}; 
-  $http.get("http://dev.markitondemand.com/MODApis/Api/v2/Quote?symbol=AAPL").success(function(data) {
-    var x2j2 = new X2JS();
-    var jsonResponse = x2j2.xml_str2json(data);
-    stock.stockQuote = jsonResponse;
-  });
+
+  this.getStockData = ['$http', function($http) {
+    console.log("WE OUT HE RHERE\n");
+    $http.get("http://dev.markitondemand.com/MODApis/Api/v2/Quote?symbol=AAPL").success(function(data) {
+      var x2j2 = new X2JS();
+      var jsonResponse = x2j2.xml_str2json(data);
+      stock.stockQuote = jsonResponse;
+    });
+  }];
 }]);
