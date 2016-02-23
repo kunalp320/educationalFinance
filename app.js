e79770c7009eda0b43b602ce1cb5ef40269b869d
Kunal Patel <kpatel@Kunals-MacBook-Pro.local>
2016-02-22 16:09:34 -0800

    Added in basic div blocks so we can display data

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
 var express = require('express');
+var path = require('path');
 var app = express();
-
+app.use(express.static(path.join(__dirname, 'public')));
 app.get('/', function(req, res) {
   res.sendFile('public/index.html', {root: __dirname});
+
 });
 
 app.listen(8080, function() {
