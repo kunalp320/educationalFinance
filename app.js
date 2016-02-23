b6f716eaa7aa7f97a6b06c802344c0850f30a76c
Kunal Patel <kpatel@Kunals-MacBook-Pro.local>
2016-02-23 15:12:32 -0800

    Cleaned up node

--- app.js
+++ app.js
@@ -12,17 +12,6 @@ app.get('/', function(req, res) {
 
 });
 
-app.post('/get_stock_quote', function(req, res) {
-  var tickerSymbol = req.body.tickerSymbol;
-  var requestURL = "http://dev.markitondemand.com/MODApis/Api/v2/Quote?symbol=" + tickerSymbol;
-  request(requestURL, function(error, response, body) {
-    if(!error && response.statusCode == 200) {
-      console.log(body);
-    }
-  });
-  console.log("We got to the endpoint with Symbol: " + tickerSymbol);
-});
-
 app.listen(8080, function() {
   console.log("Listening on port 8080");
 });
