9ffd7491f55817387e590def9d6cd40b645c9385
Kunal Patel <kpatel@Kunals-MacBook-Pro.local>
2016-02-22 17:07:14 -0800

    Added the request Pull post data

--- app.js
+++ app.js
@@ -1,14 +1,19 @@
 var express = require('express');
 var path = require('path');
+var bodyParser = require("body-parser");
 var app = express();
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
 app.use(express.static(path.join(__dirname, 'public')));
 app.get('/', function(req, res) {
   res.sendFile('public/index.html', {root: __dirname});
 
 });
 
-app.get('/get_stock_quote', function(req, res) {
+app.post('/get_stock_quote', function(req, res) {
   console.log("We got to the endpoint");
+  console.log(req.body.TickerSymbol);
 });
 
 app.listen(8080, function() {
