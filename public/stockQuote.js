f8f8cbd79e8025a1fb0eb9ade34ad7da96c01790
Kunal Patel <kpatel@Kunals-MacBook-Pro.local>
2016-02-24 18:13:29 -0800

    Chart renders, no data

--- public/stockQuote.js
+++ public/stockQuote.js
@@ -2,6 +2,7 @@ var app = angular.module('stockQuote', ['n3-line-chart']);
 
 app.controller("ChartController", ['$scope', '$http', function($scope, $http) {
   this.data = {};
+  this.hasData = false;
   var chart = this;
   this.options = {};
   var chartURL = "http://dev.markitondemand.com/MODApis/Api/v2/InteractiveChart/json?parameters=%7B%22Normalized%22%3Afalse%2C%22NumberOfDays%22%3A365%2C%22DataPeriod%22%3A%22Day%22%2C%22Elements%22%3A%5B%7B%22Symbol%22%3A%22AAPL%22%2C%22Type%22%3A%22price%22%2C%22Params%22%3A%5B%22c%22%5D%7D%5D%7D"
@@ -26,6 +27,7 @@ app.controller("ChartController", ['$scope', '$http', function($scope, $http) {
       };
       console.log(data);
       console.log(chart.options);
+      chart.hasData = true;
     });
   }
 }]);
