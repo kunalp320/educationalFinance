9fa474a91b4efc952c56e0efcc9edac3c7e15a8e
Kunal Patel <kpatel@Kunals-MacBook-Pro.local>
2016-02-24 17:53:46 -0800

    Fooling around with the charts, still not working

--- public/stockQuote.js
+++ public/stockQuote.js
@@ -1,8 +1,33 @@
-var app = angular.module('stockQuote', ['n3-chart']);
+var app = angular.module('stockQuote', []);
 
 app.controller("ChartController", ['$scope', '$http', function($scope, $http) {
   this.data = {};
+  var chart = this;
   this.options = {};
+  var chartURL = "http://dev.markitondemand.com/MODApis/Api/v2/InteractiveChart/json?parameters=%7B%22Normalized%22%3Afalse%2C%22NumberOfDays%22%3A365%2C%22DataPeriod%22%3A%22Day%22%2C%22Elements%22%3A%5B%7B%22Symbol%22%3A%22AAPL%22%2C%22Type%22%3A%22price%22%2C%22Params%22%3A%5B%22c%22%5D%7D%5D%7D"
+  this.getChartData = function() {
+    console.log("inside top");
+    $http.get(chartURL).success(function(data) {
+      console.log("inside chart");
+      chart.data = data;
+      chart.options = {
+        series: [
+          {
+            axis: "y",
+            dataset: "data",
+            key: "values",
+            label: "I dont know",
+            color: "#1f77b4",
+            type: ['line'],
+            id: 'mySeries0'
+          }
+        ],
+        axes: {x: {key: "Dates"}}
+      };
+      console.log(data);
+      console.log(chart.options);
+    });
+  }
 }]);
 
 app.controller('StockQuoteController', ['$scope', '$http', '$window', function($scope, $http, $window) {
